5c889f13861670451d7123b232f86fa0d650e027
Luguzdel <101885110+Luguzdel@users.noreply.github.com>
2022-03-18 11:53:43 -0500

    actualuzación
    se realizó el ejercicio 2

---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+base= 4
+altura=5
+area=4*5/2
+print("El área del triangulo es: ",round(area,2))
---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+capital=10000
+interes=0.035
+años=7
+vf=capital*(1+interes)**años
+print("El área de la cantidad es: ",round(vf,1))
