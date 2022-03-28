fac29627e4019ba039845d1d8ca2221f95bf2f8d
Luguzdel <101885110+Luguzdel@users.noreply.github.com>
2022-03-28 11:49:13 -0500

    actualizacion n
    ejercicio super héroes

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+radio=5
+area=3.1416*(radio**2)
+print("El área del circulo es: ",round(area,2))
+#cambio
+       
+       
---
+++ ejercicio1_28.py
@@ -0,0 +1,34 @@
+pregunta1= input ("¿Tu personaje puede volar?: ")
+pregunta2= input ("¿Tu personaje es un humano?: ")
+pregunta3= input ("¿Tu personaje tiene máscara?: ")
+
+if pregunta1== "si":
+    if pregunta2=="si":
+        if pregunta3=="si":
+            print('Ironman')
+        else:
+            print('Capitain Marvel')   
+    else: 
+        if pregunta3=="si":
+            print('Ronan Acusser')    
+        else:
+            print('Vision')    
+else: 
+    if pregunta2== "si":
+        if pregunta3=="si":
+            print('Spiderman')
+        else:
+            print('Hulk')   
+         
+    else: 
+        if pregunta3== "si":
+            print('Black Bolt')
+        else:
+            print('Thanos')
+        
+        
+        
+            
+                 
+                 
+       
